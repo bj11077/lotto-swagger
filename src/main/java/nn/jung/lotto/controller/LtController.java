6f2ba2eb60de8afebed5ab50f23dfbbc35acadda
bjp <bj11077@metabuild.co.kr>
2022-07-08 18:00:48 +0900

    요청 ip 로그추가

--- src/main/java/nn/jung/lotto/controller/LtController.java
+++ src/main/java/nn/jung/lotto/controller/LtController.java
@@ -7,10 +7,14 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import nn.jung.lotto.repo.LtRandomDto;
 import nn.jung.lotto.service.LtService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -19,6 +23,7 @@ import java.util.Map;
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
+@Slf4j
 public class LtController {
 
     private final LtService service;
@@ -30,6 +35,13 @@ public class LtController {
     @Operation(summary = "역대 로또당첨번호 조회", description = "역대 로또당첨번호")
     @GetMapping("history")
     public LtRandomDto ltHistory(@Parameter(name = "times",description = "회차",example = "1022") String times) throws IOException {
+        HttpServletRequest req = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        String ip = req.getHeader("X-FORWARDED-FOR");
+        if (ip == null)  ip = req.getRemoteAddr();
+
+        log.info("history Call");
+        log.info("request Ip : {}",ip);
+
         return service.collectData(times);
     }
 
@@ -40,6 +52,11 @@ public class LtController {
     @Operation(summary = "번호 발번", description = "no1 ~ no6 번호발번")
     @GetMapping("draw")
     public LtRandomDto ltDraw() throws IOException {
+        HttpServletRequest req = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        String ip = req.getHeader("X-FORWARDED-FOR");
+        if (ip == null)  ip = req.getRemoteAddr();
+        log.info("draw Call");
+        log.info("request Ip : {}",ip);
         return service.drawLt();
     }
 
